399598f4842dfda78f93f3c1634d61da0be84241
Daniel Mattox <dmattox10@gmail.com>
2020-01-28 14:45:44 -0500

    Placeholders done, context API

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom'
+
+import { UserContext } from './contexts/UserContext'
+
+import Login from './components/Login'
+import PrivateRoute from './components/PrivateRoute'
+import Friends from './components/Friends'
+import AddFriend from './components/AddFriend'
+import Navigation from './components/Navigation'
 
 function App() {
+
+  const [user, updateUser] = useState({})
+
   return (
     <div className="App">
-      
+      <UserContext.Provider value={{ user, updateUser }}>
+        <Navigation user={ user } />
+        <Switch>
+          <PrivateRoute path='/friends' component={ Friends } user={ user } />
+          <PrivateRoute path='/add' component={ AddFriend } user={ user } />
+          <Route path='/login' component={ Login } updateUser={ updateUser } />
+          <Route component={ Login } updateUser={ updateUser } />
+        </Switch>
+      </UserContext.Provider>
     </div>
   );
 }
